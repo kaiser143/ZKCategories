60ca83ab0770564ead349c453c6da684cb9880c0
zhangkai <deyang143@126.com>
2026-03-12 21:31:38 +0800

    no message

--- ZKCategories/Classes/CALayer/CALayer+ZKAdd.h
+++ ZKCategories/Classes/CALayer/CALayer+ZKAdd.h
@@ -56,7 +56,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic) CGFloat transformRotation;     ///< key path "tranform.rotation"
 @property (nonatomic) CGFloat transformRotationX;    ///< key path "tranform.rotation.x"
 @property (nonatomic) CGFloat transformRotationY;    ///< key path "tranform.rotation.y"
-@property (nonatomic) CGFloat transformRotationZ;   ///< key path "tranform.rotation.z"
+@property (nonatomic) CGFloat transformRotationZ;    ///< key path "tranform.rotation.z"
 @property (nonatomic) CGFloat transformScale;        ///< key path "tranform.scale"
 @property (nonatomic) CGFloat transformScaleX;       ///< key path "tranform.scale.x"
 @property (nonatomic) CGFloat transformScaleY;       ///< key path "tranform.scale.y"
